4d1f9e0b1a6a9961990d7ffae4fdd21db1ab7b1b
abqkawi1000 <chris@3fdev.com>
2016-02-22 18:21:45 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  # notify { "Kawi's box name is ${::hostname}": }
+  notify { "Kawi's box name is ${::hostname}": }
  #file { '/etc/motd':
   #noop    => true,
   #ensure  => 'file',
