b74223088cfa26b28209c78237d6f0685463207e
abqkawi1000 <chris@3fdev.com>
2016-02-23 16:30:59 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,50 @@
-Add here
+# = Class: name
+#
+# Author: Name Surname <name.surname@mail.com>
+class nginx {
+
+	package {
+	    'nginx':
+	        ensure      => installed,
+	        require     => File['doc_root'];
+	}
+
+	file {
+	    'doc_root':
+	        ensure  => directory,
+	        path    => '/var/www'
+	}
+
+	file {
+	    'nginx_conf':
+	        ensure  => file,
+	        source  => 'puppet:///modules/nginx/nginx.conf',
+	        path		=> '/etc/nginx/nginx.conf'
+	        
+	}
+
+	file {
+	    'nginx_default':
+	        ensure  => file,
+	        source  => 'puppet:///modules/nginx/default.conf',
+	        path		=> '/etc/nginx/conf.d/default.conf',
+	}
+
+	file {
+	    'index.html':
+	        ensure  => file,
+	        source  => 'puppet:///modules/nginx/index.html',
+	 }
+
+
+	service {
+	    'nginx':
+	        ensure      => running,
+	        enable      => true,
+	        subscribe   => File['nginx_conf'],
+	        require     => Package['nginx'];
+	}
+	   
+
+}
+
