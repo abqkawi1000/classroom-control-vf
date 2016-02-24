251254b7a5b8b9e2098f32b2e17ac4825a02cb09
abqkawi1000 <chris@3fdev.com>
2016-02-24 15:07:33 -0700

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,18 @@
+# Define: users::managed_user
+# Parameters:
+# arguments
+#
+define users::managed_user (
+	$group = $title,
+	) {
+	user {$title:
+		ensure => present,
+	}
+	file {"/home/${title}":
+		ensure => directory
+		owner  => $title,
+		group  => $group,
+
+}
+
+}
