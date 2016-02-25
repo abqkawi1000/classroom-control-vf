d2b23930a59d553c30297e82cebe6a3fe9e47b8e
abqkawi1000 <chris@3fdev.com>
2016-02-25 11:56:48 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,55 +1,88 @@
-# = Class: name
-#
-# Author: Name Surname <name.surname@mail.com>
-class nginx {
 
-	package {
-	    'nginx':
-	        ensure      => installed,
-	        before     => File['doc_root'],
-	}
+class nginx{
 
-	file {
-	    'doc_root':
-	        ensure  => directory,
-	        path    => '/var/www',
-	}
-
-	file {
-	    'nginx_conf':
-	        ensure  => file,
-	        source  => 'puppet:///modules/nginx/nginx.conf',
-	        path	=> '/etc/nginx/nginx.conf',
-	        require => Package['nginx'],
-	        
-	}
-
-	file {
-	    'nginx_default':
-	        ensure  => file,
-	        source  => 'puppet:///modules/nginx/default.conf',
-	        path	=> '/etc/nginx/conf.d/default.conf',
-	        require => Package['nginx'],
-	        notify  => Service['nginx'],
-	}
-
-	file {
-	    'index':
-	        ensure  => file,
-	        source  => 'puppet:///modules/nginx/index.html',
-	        path	=> '/var/www/index.html',
-	 }
-
-
-	service {
-	    'nginx':
-	        ensure      => running,
-	        enable      => true,
-	        subscribe   => File['nginx_conf'],
-	        require     => [Package['nginx'],File['nginx_conf']],
-	        #require	    => [File[config],File[block]],
-	}
-	   
-
+case $::osfamily {
+  'RedHat' : {
+    $package  = 'nginx'
+    $owner    = 'root'
+    $group    = 'root'
+    $docroot  = '/var/www'
+    $confdir  = '/etc/nginx'
+    $blockdir = '/etc/nginx/conf.d'
+    $logdir   = '/var/log/nginx'
+    $service  = 'nginx'
+    $user     = 'nginx'
+  }
+  'Debian' : {
+    $package  = 'nginx'
+    $owner    = 'root'
+    $group    = 'root'
+    $docroot  = '/var/www'
+    $confdir  = '/etc/nginx'
+    $blockdir = '/etc/nginx/conf.d'
+    $logdir   = '/var/log/nginx'
+    $service  = 'nginx'
+    $user     = 'www-data'
+  }
+  'windows' : {
+    $package  = 'nginx-service'
+    $owner    = 'Administrator'
+    $group    = 'Administrators'
+    $docroot  = 'C:/ProgramData/nginx/html'
+    $confdir  = 'C:/ProgramData/nginx'
+    $blockdir = 'C:/ProgramData/nginx/conf.d'
+    $logdir   = 'C:/ProgramData/nginx/logs'
+    $service  = 'nginx'
+    $user     = 'nobody'
+  }
+  default : {
+    fail { "Get a suppported Operating System, friend.": }
+  }
 }
 
+  File {
+    owner => $owner,
+    group => $group,
+    mode => '0664',
+  }
+
+  package { 'nginx':
+    ensure => present,
+    before => [File['config'],File['block']],
+  }
+  
+  file { 'docroot':
+    ensure => directory,
+    path => $docroot,
+  }
+  
+  file { 'index':
+    ensure => file,
+    path => "${docroot}/index.html",
+    content => template('nginx/index.html.erb'),
+  }
+    
+  file { 'config':
+    ensure => file,
+    path => "${confdir}/nginx.conf",
+    content => template('nginx/nginx.conf.erb'),
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  
+  file { 'block':
+    ensure => file,
+    path => "${blockdir}/default.conf",
+    content => template('nginx/default.conf.erb'),
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+
+  service { 'nginx':
+    ensure => running,
+    name => $service,
+    require => [File['docroot'],File['index']],
+    subscribe => [File['config'],File['block']],
+  }
+
+}
